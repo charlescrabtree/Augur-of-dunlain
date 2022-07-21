16ce5ce02083769a050d570d9b71a5088011d94d
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-07-21 16:53:15 -0700

    It works!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
 const questionSection = document.getElementById('question-section');
 const sillyQuestions = document.getElementById('silly-questions');
-const iSeekTheEye = document.getElementById('I-Seek-the-Eye-of-Magnus');
+const iSeekTheEye = document.getElementById('I-seek-the-Eye-of-Magnus');
 const answerSection = document.getElementById('answer-section');
 const questionDisplay = document.getElementById('question-display');
 const inGame = document.getElementById('in-game');
@@ -10,33 +10,35 @@ const epicQuotes = document.getElementById('epic-quotes');
 
 // let state
 const answers = [
-  
+  'yes, definitely',
+  'etc',
+  'etc etc',
+  'no, foolish mage. turn back',
 ]
 // set event listeners 
-askButton.addEventListener('click', () => {
+iSeekTheEye.addEventListener('click', () => {
   copyQuestion();
   toggleVisibility();
-  applyAnimations();
   showRandomAnswer();
 });
 
-function copyQuestion () {
-  const question = askedQuestion.value;
+function copyQuestion() {
+  const question = sillyQuestions.value;
   questionDisplay.textContent = question;
 }
 
 function toggleVisibility() {
-  question.Section.classList.add('hidden');
+  questionSection.classList.add('hidden');
   answerSection.classList.remove('hidden');
 }
 
 function showRandomAnswer() {
   const randomAnswer = getRandomItem(answers);
-  answerDisplay.textContent = randomAnswer;
+  epicQuotes.textContent = randomAnswer;
 }
 
 function getRandomItem(array) {
-  const randomInex = Math.floor(Math.random() * array.length);
+  const randomIndex = Math.floor(Math.random() * array.length);
   const item = array[randomIndex];
   return item;
 }

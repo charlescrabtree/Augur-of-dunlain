0ce569326dbd4a7141a4b68498fcfb1515785e5f
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-07-21 17:20:21 -0700

    edited answers

--- app.js
+++ app.js
@@ -10,10 +10,18 @@ const epicQuotes = document.getElementById('epic-quotes');
 
 // let state
 const answers = [
-  'yes, definitely',
-  'etc',
-  'etc etc',
-  'no, foolish mage. turn back',
+  'The trial awaits. Are you now prepared?',
+'Arrogance will serve you poorly.',
+'Step into the light, and your test begins.',
+'No.',
+'That knowledge has already been imparted to you.',
+'Like others before you, you blindly follow a path to your own destruction.',
+'You seek meaning, shelter in knowledge. You will not find it.',
+'First you must find that which you need.',
+'Events now spiral quickly towards the inevitable center, so you must act with haste.',
+'You will rely on your skill as a mage. Not on your belongings, not on your scrolls and potions.',
+'You shall find this: Knowledge will corrupt. It will destroy.',
+  
 ]
 // set event listeners 
 iSeekTheEye.addEventListener('click', () => {
